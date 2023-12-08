2d678c872208e80312a6eb7fab7af067297f4fc4
220106 <220106rq@yse-c.net>
2023-12-08 10:50:12 +0900

    update

--- resources/views/item/create.blade.php
+++ resources/views/item/create.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout>
     <div class="py-12">
+        @include('components.error')
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
